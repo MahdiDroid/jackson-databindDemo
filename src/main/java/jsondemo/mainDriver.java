6d7b956404fb76671c2b11741489217a7629d374
MahdiDroid <mehdi.mhmvd@gmail.com>
2020-02-20 14:29:58 +0300

    drverDemo class implement

--- src/main/java/jsondemo/mainDriver.java
+++ src/main/java/jsondemo/mainDriver.java
@@ -1,7 +1,23 @@
 package jsondemo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
 public class mainDriver {
     public static void main(String[] args) {
+
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+
+            Student thestudent =
+                    mapper.readValue(new File("data/sample-lite.json"),Student.class);
+            System.out.println("fistName : " + thestudent.getFirstName() +
+                                "\nlastName : " + thestudent.getLastName());
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 }
